5a30b36d0a38033dcc50192922725c63bdab8c28
Marooned13 <137838579+Marooned13@users.noreply.github.com>
2024-09-06 13:17:38 -0300

    Add files via upload

---
+++ ### JavaScript (scripts.js
@@ -0,0 +1,6 @@
+### JavaScript (scripts.js)
+javascript
+// Este código pode ser usado para funcionalidades adicionais no futuro, como abrir menus, etc.
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    alert("Menu clicked! Implementar funcionalidade de menu.");
+});
