44759755d9f76b489eb83e630a689436c6eeb9be
Bruna Adam <brunaadam54@gmail.com>
2024-09-06 17:40:06 -0300

    Exclusão dos arquivos

--- ### JavaScript (scripts.js
+++
@@ -1,6 +0,0 @@
-### JavaScript (scripts.js)
-javascript
-// Este código pode ser usado para funcionalidades adicionais no futuro, como abrir menus, etc.
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    alert("Menu clicked! Implementar funcionalidade de menu.");
-});
