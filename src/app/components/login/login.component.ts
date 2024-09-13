8791f1d79b55d934627c9e64d8b6700975b6811c
pedrogravoski <gravoski.pedro@gmail.com>
2024-09-13 20:06:27 -0300

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonIcon, IonInput, IonButton, IonInputPasswordToggle,NavController } from '@ionic/angular/standalone';
 import { addIcons } from "ionicons";
 import { HeaderComponent } from '../header/header.component';
+
 
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
-  imports: [HeaderComponent,IonIcon, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent]
+  imports: [IonButton, IonInput, HeaderComponent, IonIcon, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent,IonInputPasswordToggle]
 })
-export class LoginComponent  implements OnInit {
+export class LoginComponent implements OnInit {
 
   constructor() {
-      addIcons({}); }
+    addIcons({});
+  }
 
-  ngOnInit() {}
+ 
+
+
+
+  ngOnInit() {
+  }
 
 }
