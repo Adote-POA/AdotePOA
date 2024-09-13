bdacd8b8cbde2dfd85a85f033ac6cffa9bfab0d4
Nícolas K. <87534075+Nicolas-Kelvin@users.noreply.github.com>
2024-09-13 20:15:15 -0300

    page cadastrar animal

--- src/app/components/cadastrar-animal/cadastrar-animal.component.ts
+++ src/app/components/cadastrar-animal/cadastrar-animal.component.ts
@@ -1,19 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
-import { IonTitle, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonContent } from "@ionic/angular/standalone";
-import { addIcons } from "ionicons";
+import {
+  IonTitle,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonContent,
+  IonInput,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-cadastrar-animal',
   templateUrl: './cadastrar-animal.component.html',
   styleUrls: ['./cadastrar-animal.component.scss'],
-  imports:[HeaderComponent, IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent]
+  imports: [
+    HeaderComponent,
+    CommonModule,
+    IonInput,
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonContent,
+  ],
 })
-export class CadastrarAnimalComponent  implements OnInit {
-
-  constructor() { }
+export class CadastrarAnimalComponent implements OnInit {
+  fileToUpload: any;
+  imageUrl: any;
+  handleFileInput(event: Event) {
+    const target = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = target.files;
+    if (fileList == null) {
+      return;
+    }
+    this.fileToUpload = fileList.item(0);
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    };
+    reader.readAsDataURL(this.fileToUpload);
+  }
+  constructor() {}
 
   ngOnInit() {}
-
 }
