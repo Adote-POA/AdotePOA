f93d1d798e60dd0146171b5bd6daaa6046036bd0
Nícolas K. <87534075+Nicolas-Kelvin@users.noreply.github.com>
2024-09-19 23:17:30 -0300

    add implementação cadastrar animal

--- src/app/components/cadastrar-animal/cadastrar-animal.component.ts
+++ src/app/components/cadastrar-animal/cadastrar-animal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import {
   IonTitle,
@@ -11,6 +11,14 @@ import {
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { CommonModule } from '@angular/common';
+import { finalize } from 'rxjs';
+import { Pet } from 'src/app/models/pet';
+import { PetService } from 'src/app/services/pet.service';
+import { FormGroup, FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {Router} from '@angular/router';
+
+
+
 
 @Component({
   standalone: true,
@@ -27,11 +35,30 @@ import { CommonModule } from '@angular/common';
     IonMenuButton,
     IonTitle,
     IonContent,
+    FormsModule, 
+    ReactiveFormsModule,
   ],
 })
-export class CadastrarAnimalComponent implements OnInit {
+
+
+export class CadastrarAnimalComponent {
   fileToUpload: any;
   imageUrl: any;
+  petService = inject(PetService);
+  _router = inject(Router);
+
+  petForm = new FormGroup({
+    nome: new FormControl(''),
+    idade: new FormControl(''),
+    raca: new FormControl(''),
+    pelagem: new FormControl(''),
+    vacinas: new FormControl(true),
+    temperamento: new FormControl(''),
+    porte: new FormControl(''),
+    descricao: new FormControl(''),
+    endereco: new FormControl(''),
+  });
+
   handleFileInput(event: Event) {
     const target = event.currentTarget as HTMLInputElement;
     let fileList: FileList | null = target.files;
@@ -47,5 +74,9 @@ export class CadastrarAnimalComponent implements OnInit {
   }
   constructor() {}
 
-  ngOnInit() {}
+  onSubmit(){
+    //novoPet:Pet = this.petForm.value as Pet;
+    this.petService.newPet(this.petForm.value);
+    this._router.navigate([''])
+  }
 }
