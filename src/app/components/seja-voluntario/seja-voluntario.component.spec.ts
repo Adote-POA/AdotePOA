29ab52aaa19b751f42847933ddc495f7c29c71c8
NeisGuilherme <128760708+NeisGuilherme@users.noreply.github.com>
2024-09-20 13:51:51 -0300

    alterações seja-voluntário

--- src/app/components/seja-voluntario/seja-voluntario.component.spec.ts
+++ src/app/components/seja-voluntario/seja-voluntario.component.spec.ts
@@ -28,4 +28,6 @@ export class SejaVoluntarioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() { }
+
+  
 }
--- src/app/components/seja-voluntario/seja-voluntario.component.ts
+++ src/app/components/seja-voluntario/seja-voluntario.component.ts
@@ -18,3 +18,4 @@ export class SejaVoluntarioComponent  implements OnInit {
   ngOnInit() {}
 
 }
+
